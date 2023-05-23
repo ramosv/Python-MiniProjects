f00f342b5068c506fd3e7300a1433f27692e6bc1
ramosv <56368263+ramosv@users.noreply.github.com>
2023-05-22 18:35:08 -0600

    Generator

---
+++ generator_Yields.py
@@ -0,0 +1,20 @@
+## With in a range print all numbers that are prime using yield
+def isPrime(x):
+    if x < 2:
+        return False
+    elif x == 2:
+        return True
+    for n in range(2,x):
+        if x % n ==0:
+            return False
+    return True
+
+def primeGenerator(x,y):
+    for n in range(x,y):
+        if isPrime(n) is True:
+            yield n
+
+start = int(input())
+end = int(input())
+
+print(list(primeGenerator(start,end)))
