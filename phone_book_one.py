75f78ffe575276a27a92414e82568200bb3ce610
ramosv <56368263+ramosv@users.noreply.github.com>
2023-05-29 01:09:00 -0600

    Phone book finished but not prety

--- phone_book_one.py
+++ phone_book_one.py
@@ -1,17 +1,19 @@
 phonebook = {}
 
 while True:
-    command = input("1 search, 2 add, 3 quit")
-    if command == 3:
+    command = input("command(1 search, 2 add, 3 quit):")
+    if command == "3":
         print("quitting...")
+        break
     
-    elif command == 1:
-        name = input()
+    elif command == "1":
+        name = input("name:")
         if name in phonebook:
             print(phonebook[name])
         else:
             print("no number")
-    elif command == 2:
-        nam = input()
-        numer = input()
+    elif command == "2":
+        nam = input("name:")
+        numer = input("number")
         phonebook[nam] = numer
+        print("ok!")
